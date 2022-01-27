42c5ef1209e98bc7298c621a2827c2e127a8546f
Alexander <chernikov.av@edu.spbstu.ru>
2022-01-28 00:47:03 +0300

    fix: replace directory app

--- online_journal/online_journal/settings.py
+++ online_journal/online_journal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)$irag$5ltvr@&tc)$dp$!ijqn2hag%b2=#makz*irx+)g4g@v'
+SECRET_KEY = 'django-insecure-x$a^41^c+7$kr158l-xb9*49qh6l9)6s4d^)zdcdl@)utn5tql'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
--- online_journal/online_journal/urls.py
+++ online_journal/online_journal/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
+
+from datetime import datetime
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),
+    path('contact/', include('app.urls')),
+    path('about/',  include('app.urls')),
+    path('login/', include('app.urls')),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
 ]
