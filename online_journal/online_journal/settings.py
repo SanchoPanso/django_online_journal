be72612297b98c6981290fdf04963ae525834bb6
Alexander <chernikov.av@edu.spbstu.ru>
2022-01-27 21:32:31 +0300

    chore: add the first html's

--- online_journal/online_journal/settings.py
+++ online_journal/online_journal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#ia7wzb0_w*!%inyo#z=z^f59a91(7!zjh2-r9+69kxaqyl1or'
+SECRET_KEY = 'django-insecure-)$irag$5ltvr@&tc)$dp$!ijqn2hag%b2=#makz*irx+)g4g@v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
 ]
 
 MIDDLEWARE = [
